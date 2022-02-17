b625f07c9fe7031c9a56fb3fb8d60e0627cde8ae
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-02-17 11:25:55 +0100

    Split into modules
    For a better code structure, split the single file code into modules.
    This will also help testing.

--- obs_maven/artifact.py
+++ obs_maven/artifact.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-
 # Tool creating a maven repository out of rpms built by OBS
-# Copyright (C) 2019  SUSE Inc.
+# Copyright (C) 2022  SUSE Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 
-import argparse
 from datetime import datetime
 import errno
 import logging
@@ -23,162 +20,8 @@ import os
 import os.path
 import re
 import shutil
-import sys
 import subprocess
-import tempfile
-import urllib.request
 import xml.etree.ElementTree as ET
-import yaml
-import zlib
-
-
-logging.basicConfig(level=logging.INFO)
-
-
-# RPM version comparison shamelessly taken from:
-# https://stackoverflow.com/questions/3206319/how-do-i-compare-rpm-versions-in-python/42967591#42967591
-try:
-    from rpm import labelCompare as _compare_rpm_labels
-except ImportError:
-    # Emulate RPM field comparisons
-    #
-    # * Search each string for alphabetic fields [a-zA-Z]+ and
-    #   numeric fields [0-9]+ separated by junk [^a-zA-Z0-9]*.
-    # * Successive fields in each string are compared to each other.
-    # * Alphabetic sections are compared lexicographically, and the
-    #   numeric sections are compared numerically.
-    # * In the case of a mismatch where one field is numeric and one is
-    #   alphabetic, the numeric field is always considered greater (newer).
-    # * In the case where one string runs out of fields, the other is always
-    #   considered greater (newer).
-
-    logging.warning("Failed to import 'rpm', emulating RPM label comparisons")
-
-    from itertools import zip_longest
-
-    _subfield_pattern = re.compile(
-        r'(?P<junk>[^a-zA-Z0-9]*)((?P<text>[a-zA-Z]+)|(?P<num>[0-9]+))'
-    )
-
-    def _iter_rpm_subfields(field):
-        """Yield subfields as 2-tuples that sort in the desired order
-
-        Text subfields are yielded as (0, text_value)
-        Numeric subfields are yielded as (1, int_value)
-        """
-        for subfield in _subfield_pattern.finditer(field):
-            text = subfield.group('text')
-            if text is not None:
-                yield (0, text)
-            else:
-                yield (1, int(subfield.group('num')))
-
-    def _compare_rpm_field(lhs, rhs):
-        # Short circuit for exact matches (including both being None)
-        if lhs == rhs:
-            return 0
-        # Otherwise assume both inputs are strings
-        lhs_subfields = _iter_rpm_subfields(lhs)
-        rhs_subfields = _iter_rpm_subfields(rhs)
-        for lhs_sf, rhs_sf in zip_longest(lhs_subfields, rhs_subfields):
-            if lhs_sf == rhs_sf:
-                # When both subfields are the same, move to next subfield
-                continue
-            if lhs_sf is None:
-                # Fewer subfields in LHS, so it's less than/older than RHS
-                return -1
-            if rhs_sf is None:
-                # More subfields in LHS, so it's greater than/newer than RHS
-                return 1
-            # Found a differing subfield, so it determines the relative order
-            return -1 if lhs_sf < rhs_sf else 1
-        # No relevant differences found between LHS and RHS
-        return 0
-
-
-    def _compare_rpm_labels(lhs, rhs):
-        lhs_epoch, lhs_version, lhs_release = lhs
-        rhs_epoch, rhs_version, rhs_release = rhs
-        result = _compare_rpm_field(lhs_epoch, rhs_epoch)
-        if result:
-            return result
-        result = _compare_rpm_field(lhs_version, rhs_version)
-        if result:
-            return result
-        return _compare_rpm_field(lhs_release, rhs_release)
-
-
-class Rpm:
-    def __init__(self, location, mtime, name, version_node):
-        self.path = location
-        self.mtime = mtime
-        self.name = location[location.find('/') + 1:]
-        self.pkgname = name
-        self.epoch = version_node.get('epoch')
-        self.version = version_node.get('ver')
-        self.release = version_node.get('rel')
-
-    def compare(self, other):
-        return _compare_rpm_labels(
-                (other.epoch, other.version, other.release),
-                (self.epoch, self.version, self.release))
-
-
-class Repo:
-    def __init__(self, uri, project, repository):
-        self.uri = uri
-        self.project = project.replace(':', ':/')
-        self.repository = repository
-        self._rpms = None
-
-    def find_primary(self):
-        ns = {'repo': 'http://linux.duke.edu/metadata/repo', 'rpm': 'http://linux.duke.edu/metadata/rpm'}
-        f = urllib.request.urlopen(
-            "{}/{}/{}/repodata/repomd.xml".format(self.uri, self.project, self.repository)
-        )
-        doc = ET.fromstring(f.read())
-        primary_href = doc.find("./repo:data[@type='primary']/repo:location", ns).get("href")
-        return "{}/{}/{}/{}".format(self.uri, self.project, self.repository, primary_href)
-
-    def parse_primary(self):
-        ns = {"c": "http://linux.duke.edu/metadata/common", "rpm":"http://linux.duke.edu/metadata/rpm"}
-        f = urllib.request.urlopen(self.find_primary())
-        primary_xml = zlib.decompress(f.read(), 16 + zlib.MAX_WBITS)
-        doc = ET.fromstring(primary_xml)
-
-        all_rpms = [
-            Rpm(n.find("c:location", ns).get("href"),
-                int(n.find("c:time", ns).get("file")),
-
-                n.find("c:name", ns).text,
-                n.find("c:version", ns))
-            for n
-            in doc.findall(".//c:package", ns)
-            if n.find("c:arch", ns).text in ["x86_64", "noarch"]
-        ]
-        latest_rpms = {}
-        for rpm in all_rpms:
-            latest = latest_rpms.get(rpm.pkgname)
-            if latest is None or latest.compare(rpm):
-                latest_rpms[rpm.pkgname] = rpm
-        self._rpms = latest_rpms.values()
-
-    @property
-    def rpms(self):
-        if not self._rpms:
-            self.parse_primary()
-        return self._rpms
-
-    def get_binary(self, path, target, mtime):
-        """
-        Equivalent of osc.core.get_binary_file
-        """
-        f = urllib.request.urlopen("{}/{}/{}/{}".format(self.uri, self.project, self.repository, path))
-        target_f = open(target, 'wb')
-        shutil.copyfileobj(f, target_f)
-        target_f.close()
-        f.close()
-        os.utime(target, (mtime, mtime))
 
 
 class Artifact:
@@ -378,58 +221,3 @@ class Artifact:
             self.deploy(jar, version, repo, file.mtime)
         else:
             logging.info('Skipping artifact %s' % self.artifact)
-
-
-class Configuration:
-    def __init__(self, config_path, repo):
-        data = {}
-        if os.path.isfile(config_path):
-            f = open(config_path, 'r')
-            data = yaml.safe_load(f)
-            f.close()
-        self.url = data.get('url', "https://download.opensuse.org/repositories")
-        self.repo = repo
-        repositories = data.get('repositories', {})
-        repos = {
-            name: Repo(self.url, data['project'], data['repository'])
-            for name, data
-            in repositories.items()
-        }
-
-        self.artifacts = [Artifact(artifact, repos, data.get('group', 'suse')) for artifact in data.get('artifacts', [])]
-
-def main():
-    ret = 0
-    parser = argparse.ArgumentParser(
-        description="OBS to Maven repository synchronization tool",
-        conflict_handler='resolve',
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    parser.add_argument('config', help="Path to the YAML configuration file")
-    parser.add_argument('out', help="Path to the output maven repository")
-
-    parser.add_argument(
-        "-d", "--debug",
-        help="Show debug messages",
-        action="store_const",
-        dest="loglevel",
-        const=logging.DEBUG,
-        default=logging.INFO)
-
-    args = parser.parse_args()
-
-    logging.getLogger().setLevel(args.loglevel)
-    logging.debug('Reading configuration')
-    config = Configuration(args.config, args.out)
-    tmp = tempfile.mkdtemp(prefix="obsmvn-")
-    try:
-        for artifact in config.artifacts:
-            artifact.process(config.repo, tmp)
-    except RuntimeError as e:
-        logging.error(e)
-        ret = 1
-    shutil.rmtree(tmp)
-    return ret
-
-if __name__ == '__main__':
-    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="obs-to-maven",
+    version="1.0.0",
+    author="Cedric Bosdonnat",
+    author_email="cedric.bosdonnat@suse.com",
+    description="Tool extracting jars from RPMs in OpenBuildService to a maven repo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uyuni-project/obs-to-maven",
+    packages=["obs_maven"],
+    entry_points={"console_scripts": ["obs-to-maven = obs_maven.core:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["PyYAML"],
+)
