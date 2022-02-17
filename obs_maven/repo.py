5ead2c6a6111a24590e97fff3fb8c6f2b96c0f3b
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-02-17 11:37:00 +0100

    Parse repo primary.xml file using SAX
    The primary.xml file contains all the metadata of all the rpms in the
    repository... and this can grow big.
    
    With the configuration from uyuni which uses the Leap repo, the max
    memory used by obs-to-maven was 2.6GB. Switching to SAX reduces the
    amount of needed memory to 283MB!

--- obs_maven/repo.py
+++ obs_maven/repo.py
@@ -13,13 +13,18 @@
 #
 # You should have received a copy of the GNU General Public License
 
+import io
+import logging
 import os
-import os.path
 import shutil
 import urllib.request
+import xml.sax
+import xml.sax.handler
 import xml.etree.ElementTree as ET
+from xml.sax.xmlreader import InputSource
 import zlib
 
+import obs_maven.primary_handler
 from obs_maven.rpm import Rpm
 
 class Repo:
@@ -31,35 +36,26 @@ class Repo:
 
     def find_primary(self):
         ns = {'repo': 'http://linux.duke.edu/metadata/repo', 'rpm': 'http://linux.duke.edu/metadata/rpm'}
-        f = urllib.request.urlopen(
-            "{}/{}/{}/repodata/repomd.xml".format(self.uri, self.project, self.repository)
-        )
+        repomd_url = "{}/{}/{}/repodata/repomd.xml".format(self.uri, self.project, self.repository)
+        logging.debug("Parsing %s", repomd_url)
+        f = urllib.request.urlopen(repomd_url)
         doc = ET.fromstring(f.read())
         primary_href = doc.find("./repo:data[@type='primary']/repo:location", ns).get("href")
         return "{}/{}/{}/{}".format(self.uri, self.project, self.repository, primary_href)
 
     def parse_primary(self):
-        ns = {"c": "http://linux.duke.edu/metadata/common", "rpm":"http://linux.duke.edu/metadata/rpm"}
-        f = urllib.request.urlopen(self.find_primary())
-        primary_xml = zlib.decompress(f.read(), 16 + zlib.MAX_WBITS)
-        doc = ET.fromstring(primary_xml)
-
-        all_rpms = [
-            Rpm(n.find("c:location", ns).get("href"),
-                int(n.find("c:time", ns).get("file")),
-
-                n.find("c:name", ns).text,
-                n.find("c:version", ns))
-            for n
-            in doc.findall(".//c:package", ns)
-            if n.find("c:arch", ns).text in ["x86_64", "noarch"]
-        ]
-        latest_rpms = {}
-        for rpm in all_rpms:
-            latest = latest_rpms.get(rpm.pkgname)
-            if latest is None or latest.compare(rpm):
-                latest_rpms[rpm.pkgname] = rpm
-        self._rpms = latest_rpms.values()
+        primary_url = self.find_primary()
+        logging.debug("Parsing primary %s", primary_url)
+        with urllib.request.urlopen(primary_url) as primary_fd:
+            primary_xml = zlib.decompress(primary_fd.read(), 16 + zlib.MAX_WBITS)
+            parser = xml.sax.make_parser()
+            handler = obs_maven.primary_handler.Handler()
+            parser.setContentHandler(handler)
+            parser.setFeature(xml.sax.handler.feature_namespaces, True)
+            input_source = InputSource()
+            input_source.setByteStream(io.BytesIO(primary_xml))
+            parser.parse(input_source)
+            self._rpms = handler.rpms.values()
 
     @property
     def rpms(self):
